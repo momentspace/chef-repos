246d8575e480449f94dfd9f105efa367a8825321
momentspace <service@starferry.net>
2014-08-03 13:57:37 +0900

    recipeのデバッグおよびvimrcはdotfileからダウンロードするように変更

--- site-cookbooks/vim/recipes/default.rb
+++ site-cookbooks/vim/recipes/default.rb
@@ -10,14 +10,35 @@ package "vim" do
   action :install
 end
 
-rcfile = File.join(node[:vim][:dir], ".vimrc")
-template rcfile do
-  source "vimrc.erb"
-end
-
-file rcfile do
+vim_dir = File.join(node[:vim][:dir], ".vim")
+directory vim_dir do
+  not_if { File.exists? vim_dir }
+  action :create
+  mode 0755
   owner node[:vim][:user]
   group node[:vim][:group]
 end
 
+neobundle_dir = File.join(node[:vim][:dir], ".vim/bundle")
+directory neobundle_dir do
+  not_if { File.exists? neobundle_dir }
+  action :create
+  mode 0755
+  owner node[:vim][:user]
+  group node[:vim][:group]
+end
+
+neobundle_file = File.join(neobundle_dir, "neobundle.vim")
+git neobundle_file do
+  repository "https://github.com/Shougo/neobundle.vim"
+  action :sync
+  user node[:vim][:user]
+  group node[:vim][:group]
+end
+
+execute "vim +':NeoBundleInstall' +:q" do
+  action :run
+  user node[:vim][:user]
+  group node[:vim][:group]
+end
 
