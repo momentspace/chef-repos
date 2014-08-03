19eac5e30259163b654e84de831fffb18bf1ce97
momentspace <service@starferry.net>
2014-08-03 15:15:01 +0900

    dotfilesのcookbookを追加。neobundleの自動実行を一時中断

---
+++ site-cookbooks/dotfiles/attributes/default.rb
@@ -0,0 +1,3 @@
+default[:dotfiles][:dir] = "/home/moment"
+default[:dotfiles][:user] = "moment"
+default[:dotfiles][:group] = "moment"
---
+++ site-cookbooks/dotfiles/recipes/default.rb
@@ -0,0 +1,25 @@
+#
+# Cookbook Name:: dotfiles
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+dotfiles_repos = "https://github.com/momentspace/dotfiles.git"
+git node[:dotfiles][:dir] do
+  repository dotfiles_repos
+  action :sync
+  user node[:dotfiles][:user]
+  group node[:dotfiles][:group]
+end
+
+shell = File.join(node[:dotfiles][:dir], "dotfiles/configure.sh")
+execute shell do
+  action :run
+  user node[:dotfiles][:user]
+  group node[:dotfiles][:group]
+  ignore_failure true
+end
+
